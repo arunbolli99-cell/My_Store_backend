dbadf4f2ed6e697274b4db5f8dcccc11c9cb2c04
arunbolli <arunbolli99@gmail.com>
2025-12-22 23:13:37 +0530

    Added CORS configuration to allow frontend on Vercel

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 dotenv.config();
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://my-store-backend-h6ho.onrender.com',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
