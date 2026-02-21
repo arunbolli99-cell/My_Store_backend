06108adf6e0b96895e30dffde199a6cc8459b3e5
arunbolli <arunbolli99@gmail.com>
2026-02-21 20:12:45 +0530

    fixed the bug

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 
 // CORS
 app.use(cors({
-  origin: 'https://my-store-project-1alr9kpvl-arunbolli99-cells-projects.vercel.app',
+  origin: 'https://my-store-project-p9ch11dke-arunbolli99-cells-projects.vercel.app',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
